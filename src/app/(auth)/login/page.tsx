160d722610b2410eb83e117327e10c6517ad2167
Habeeb <habeebahmadu1@gmail.com>
2024-03-08 22:06:11 +0100

    feat: add login page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "@/components/forms/login-form";
+
+const LoginPage = () => {
+  return (
+    <div className="w-full h-full wrapper flex items-center justify-center">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
