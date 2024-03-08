d6b3e1104f4fcd6e7ca4141a6198de2046e62bbf
Habeeb <habeebahmadu1@gmail.com>
2024-03-08 22:05:30 +0100

    feat: add register page

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components";
+
+const RegisterPage = () => {
+  return (
+    <div className="w-full h-full wrapper flex items-center justify-center">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;
